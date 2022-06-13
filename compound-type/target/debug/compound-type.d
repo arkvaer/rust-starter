ab6256ca469bd3c6f0ce4f45734d3d87cd015cc9
arkvaer <arkvaer@gmail.com>
2022-06-13 10:31:28 +0800

    commit

---
+++ compound-type/target/debug/compound-type.d
@@ -0,0 +1,1 @@
+E:\Github\rust-starter\compound-type\target\debug\compound-type.exe: E:\Github\rust-starter\compound-type\src\main.rs
---
+++ compound-type/target/debug/deps/compound_type-1594ca0fc9d7fa19.d
@@ -0,0 +1,5 @@
+E:\Github\rust-starter\compound-type\target\debug\deps\compound_type-1594ca0fc9d7fa19.exe: src\main.rs
+
+E:\Github\rust-starter\compound-type\target\debug\deps\compound_type-1594ca0fc9d7fa19.d: src\main.rs
+
+src\main.rs:
